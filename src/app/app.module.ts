9b995f30b33240728d93c1ce8a4ad00db9e3e299
PEDRO <pedropan2906@hotmail.com>
2024-04-03 12:34:39 -0500

    html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideDatabase(() => getDatabase()),
     provideFirebaseApp(() => initializeApp({"projectId":"rtdb-expl-8cffc","appId":"1:867479733134:web:2d3b6c9c4298345b6a1adf","databaseURL":"https://rtdb-expl-8cffc-default-rtdb.firebaseio.com","storageBucket":"rtdb-expl-8cffc.appspot.com","apiKey":"AIzaSyDqQd7DgXjISmqdwXQ7SThZ1kB520W0-Es","authDomain":"rtdb-expl-8cffc.firebaseapp.com","messagingSenderId":"867479733134"}))],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
